fdb7194598aa2064a0a6d9b4601495870f13ed3c
Abdullahi30 <124450812+Abdullahi30@users.noreply.github.com>
2023-02-05 09:53:41 +0300

    Update splash_screen.dart

--- Institute-Mangement/lib/screens/splash_screen/splash_screen.dart
+++ Institute-Mangement/lib/screens/splash_screen/splash_screen.dart
@@ -1,4 +1,4 @@
-import 'package:brain_school/screens/login_screen/login_screen.dart';
+import 'package:Institute-Mangement/screens/login_screen/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
